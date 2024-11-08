d773ef1ef424193559ab1f4a4e4d7f0c87785bbc
EvgeniyaYakunina <Yakoonina@gmail.com>
2024-11-08 13:47:32 +0300

    fix notification sorting

--- src/features/notifications/api/notificationsApi.ts
+++ src/features/notifications/api/notificationsApi.ts
@@ -29,19 +29,6 @@ export const notificationsApi = baseApi.injectEndpoints({
     getAllNotifications: builder.query<GetNotificationsResponse, GetNotificationsArgs>({
       providesTags: ['Notifications'],
       query: ({ cursor }) => GetAllNotifications(cursor),
-      /**
-       * Transforms the response from the API by sorting the notifications
-       * based on the notification date in ascending order.
-       * @param {GetNotificationsResponse} response - The response object from the API containing
-       * the notifications data and metadata.
-       */
-      transformResponse: (response: GetNotificationsResponse) => {
-        response.items.sort(
-          (a, b) => new Date(a.notifyAt).getTime() - new Date(b.notifyAt).getTime()
-        )
-
-        return response
-      },
     }),
     /** Marks a list of notifications as read.
      * @param {number[]} params.ids - Array of notification IDs to mark as read.
